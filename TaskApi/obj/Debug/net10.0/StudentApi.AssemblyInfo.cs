347785020f0069c07b4ccca2a58b731f928c907f
Lê Anh Vinh <127384840+Le-Anh-Vinh@users.noreply.github.com>
2025-11-19 12:58:44 +0700

    Debug

--- TaskApi/obj/Debug/net10.0/StudentApi.AssemblyInfo.cs
+++ TaskApi/obj/Debug/net10.0/StudentApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+381d7e948d6fb6ae9233fa2fd9b6af1088ffc164")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abca8ec27511969385e1ba875cb5fbb69b45fd44")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
